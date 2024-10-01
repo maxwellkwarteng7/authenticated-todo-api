68942131dbd30f9aa49f114f7c35262dbd647286
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2024-10-01 11:12:02 +0000

    --added the authenticator middleware for verifying user token --

---
+++ routes/dashboard.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/dashboard', (req, res) => {
+    const { userId } = req;
+
+    res.status(200).json({ msg: "this user is authenticated" });
+});
+
+
+
+module.exports = router; 
